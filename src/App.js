57c49c10c292dac75893037bbb0fd6053ea89d43
paul kayongo <nserekopaul@gmail.com>
2019-07-24 14:33:25 +0300

    Delete yarn.lock

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import React from 'react';
-import Header from './components/Header';
-import {
-    BrowserRouter as Router,
-    Route,
-    Switch
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store/combineStore'
+
+import Header from './containers/Header';
+import Footer from './components/Footer';
+import GetAllArticlesView from './containers/GetAllArticlesView';
+import LoginView from './containers/auth/LoginView';
+import RegistrationView from './containers/auth/RegistrationView';
+
 import NotFoundPage from './components/NotFoundPage';
+
 
 function App() {
     return (
-        <div>
-
+        <Provider store={store}>
             <Router>
                 <Header />
                 <Switch>
+                    <Route exact path="/" component={GetAllArticlesView} />
+                    <Route exact path="/login" component={LoginView} />
+                    <Route
+                        exact
+                        path="/register"
+                        component={RegistrationView}
+                    />
                     <Route component={NotFoundPage} />
                 </Switch>
+                <Footer />
             </Router>
-        </div>
+        </Provider>
     );
 }
 export default App;
--- src/containers/Header.js
+++ src/containers/Header.js
@@ -2,26 +2,14 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 export class Header extends Component {
+
     render() {
         return (
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
+                <div className="container-fluid">
                 <Link to="/" className="navbar-brand" href="#">
                     Listicle
                 </Link>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-toggle="collapse"
-                    data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon" />
-                </button>
-
-
                     <div className="navbar--center">
                         <form className="form-inline">
                             <input
@@ -37,13 +25,20 @@ export class Header extends Component {
                             className="btn btn-basic my-2 my-sm-0"
                             type="submit"
                         >
-                            <span className="small-font">LOGIN</span>
+                            <Link to="/login" className="auth-login"> <span className="small-font">PROFILES</span></Link>
+                        </button>
+
+                        <button
+                            className="btn btn-basic my-2 my-sm-0"
+                            type="submit"
+                        >
+                            <Link to="/login" className="auth-login"> <span className="small-font">LOGIN</span></Link>
                         </button>
                         <button
                             className="btn btn-outline-primary my-2 my-sm-0"
                             type="submit"
                         >
-                            <span className="small-font">GET STARTED</span>
+                            <Link to="/register" className="auth-register"><span className="small-font">GET STARTED</span></Link>
                         </button>
                     </div>
                 </div>
